8fad342b0c98b7d732b0ea1f9a739673e9e8ae86
Jansu2001 <sadnansujan23@gmail.com>
2023-02-18 17:27:26 +0600

    first commit

---
+++ js/questions.js
@@ -0,0 +1,47 @@
+//Questions and Options array
+
+const quizArray = [
+    {
+      id: "0",
+      question: "Which is the most widely spoken language in the world?",
+      options: ["Spanish", "Mandarin", "English", "German"],
+      correct: "Mandarin",
+    },
+    {
+      id: "1",
+      question: "Which is the only continent in the world without a desert?",
+      options: ["North America", "Asia", "Africa", "Europe"],
+      correct: "Europe",
+    },
+    {
+      id: "2",
+      question: "Who invented Computer?",
+      options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
+      correct: "Charles Babbage",
+    },
+    {
+      id: "3",
+      question: "Who is The Founder of Apple?",
+      options: ["Mark Zukarburg", "Lary Page", "Sundar Pichai", "Stev Jobs"],
+      correct: "Stev Jobs",
+    },
+    {
+      id: "4",
+      question: "ই-কমার্স সাইট amazon.com কত সালে প্রতিষ্ঠিত হয়?",
+      options: ["১৯৯০ সালে", "১৯৮৮ সালে", "১৯৯৪ সালে", "১৯৯৮ সালে"],
+      correct: "১৯৯৪ সালে",
+    },
+
+    {
+      id: "5",
+      question: "আইওএস (IOS) মোবাইল অপারেটিং সিস্টেমটি কোন প্রতিষ্ঠান বাজারজাত করে?",
+      options: ["অ্যাপেল", , "গুগল", "মাইক্রোসফট", "আইডিএম"],
+      correct: "অ্যাপেল",
+    },
+    {
+      id: "6",
+      question: "বাংলাদেশ কবে আইসিসির সহযোগী সদস্য পদ লাভ করে?",
+      options: ["১৯৭৭", , "১৯৭৫", "১৯৭৯", "১৯৭২"],
+      correct: "১৯৭৭",
+    },
+  ];
